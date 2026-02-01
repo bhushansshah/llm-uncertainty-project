a670b8c1e84983c81ceb29fa963c93288412f874
bhushansshah <bhushan.shah05@gmail.com>
2026-02-01 21:34:45 +0000

    Adding comments and updating the cleaning code for the generations

--- semantic_uncertainty/code/clean_generated_strings.py
+++ semantic_uncertainty/code/clean_generated_strings.py
@@ -10,10 +10,14 @@ from transformers import AutoTokenizer
 
 import config
 import wandb
+from dotenv import load_dotenv
+load_dotenv()
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--generation_model', type=str, default='opt-350m')
 parser.add_argument('--run_id', type=str, default='run_1')
+parser.add_argument('--project', type=str, default='opt_350m')
+parser.add_argument('--dataset', type=str, default='trivia_qa')
 args = parser.parse_args()
 
 device = 'cuda'
@@ -33,47 +37,48 @@ np.random.seed(seed_value)
 #Fix torch random seed
 torch.manual_seed(seed_value)
 
-os.environ["HF_DATASETS_CACHE"] = config.hf_datasets_cache
+os.environ["HF_DATASETS_CACHE"] = config.hf_datasets_cache #Set the cache directory for the datasets
 
-generation_tokenizer = AutoTokenizer.from_pretrained(f"facebook/opt-350m", use_fast=False, cache_dir=config.data_dir)
+generation_tokenizer = AutoTokenizer.from_pretrained(f"facebook/{args.generation_model}", use_fast=False, cache_dir=config.data_dir) #Load the generation tokenizer
 
-wandb.init(project='nlg_uncertainty_opt_350m', id=args.run_id, config=args, resume='allow')
+wandb.init(project=args.project, id=args.run_id, config=args, resume='allow') #Initialize wandb
 
 run_name = wandb.run.name
 
-tokenizer = AutoTokenizer.from_pretrained(f"facebook/opt-350m", use_fast=False, cache_dir=config.data_dir)
+tokenizer = AutoTokenizer.from_pretrained(f"facebook/{args.generation_model}", use_fast=False, cache_dir=config.data_dir) #Load the tokenizer
 
-with open(f'{config.output_dir}/{run_name}/{args.generation_model}_generations.pkl', 'rb') as infile:
+with open(f'{config.output_dir}/sequences/{run_name}/{args.dataset}_{args.generation_model}_generations_small.pkl', 'rb') as infile: #Load the generations
     sequences = pickle.load(infile)
 
-cleaned_sequences = []
+cleaned_sequences = [] #Initialize the cleaned sequences
 
 for sample in tqdm(sequences):
-    cleaned_generations = torch.ones_like(sample['generations'])
-    question = sample['question']
-    generated_texts = sample['generated_texts']
-    cleaned_generated_texts = []
+    cleaned_generations = torch.ones_like(sample['generations']) #Initialize the cleaned generations to same shape as the generations (this contains the token ids of the entire prompt and the completion)
+    question = sample['question'] #Get the question (text)
+    generated_texts = sample['generated_texts'] #Get the generated texts (only contains the text of the completion)
+    cleaned_generated_texts = [] #Initialize the cleaned generated texts
 
-    max_len_of_generations = cleaned_generations.shape[-1]
+    max_len_of_generations = cleaned_generations.shape[-1] #Get the maximum length of the generations
 
-    strings_to_filter_on = [
+    strings_to_filter_on = [ #List of strings to filter on
         '.', '\n', 'Q:', 'A:', 'question:', 'answer:', 'Question:', 'Answer:', 'Questions:', 'questions:', 'QUESTION:',
         'ANSWER:'
     ]
 
-    for i, generated_text in enumerate(generated_texts):
-        for string in strings_to_filter_on:
+    for i, generated_text in enumerate(generated_texts): #For each generated text
+        for string in strings_to_filter_on: #For each string to filter on
             if string in generated_text:
-                generated_text = generated_text.split(string)[0]
-        cleaned_generated_texts.append(generated_text)
-        clean_ids = torch.cat(
+                generated_text = generated_text.split(string)[0] #Split the generated text at the string and take the part before the string
+        cleaned_generated_texts.append(generated_text) #Append the cleaned generated text to the list
+        clean_ids = torch.cat( #Concatenate the prompt and the generated text. We remove the first token of the generated text as it is the </s>token.
             [sample['prompt'].to(device),
-             torch.tensor(tokenizer(generated_text)['input_ids'][1:], device=device)])
-        cleaned_generations[i, :min(len(clean_ids), max_len_of_generations)] = clean_ids[:max_len_of_generations]
+             torch.tensor(tokenizer(generated_text)['input_ids'][1:], device=device)]) #Tokenize the generated text and concatenate with the prompt
+        cleaned_generations[i, :min(len(clean_ids), max_len_of_generations)] = clean_ids[:max_len_of_generations] #Set the cleaned generations to the cleaned ids
 
-    sample['cleaned_generated_texts'] = cleaned_generated_texts
-    sample['cleaned_generations'] = cleaned_generations
-    cleaned_sequences.append(sample)
+    sample['cleaned_generated_texts'] = cleaned_generated_texts #Set the cleaned generated texts to the list
+    sample['cleaned_generations'] = cleaned_generations #Set the cleaned generations to the tensor
+    cleaned_sequences.append(sample) #Append the cleaned sample to the list
 
-with open(f'{config.output_dir}/{run_name}/{args.generation_model}_generations.pkl', 'wb') as outfile:
-    pickle.dump(cleaned_sequences, outfile)
+with open(f'{config.output_dir}/sequences/{run_name}/{args.dataset}_{args.generation_model}_generations_small.pkl', 'wb') as outfile: #Save the cleaned sequences
+    pickle.dump(cleaned_sequences, outfile) #Save the cleaned sequences
+print("The cleaned sequences are saved in the pickle file")
