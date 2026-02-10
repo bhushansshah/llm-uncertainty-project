aade3a71329a47197de5f1c3f0229e8346c0bb10
bhushansshah <bhushan.shah05@gmail.com>
2026-02-10 13:34:19 -0500

    Adding code and results for Average Token Entropy Baseline.

--- baselines.py
+++ baselines.py
@@ -1,4 +1,5 @@
 import numpy as np
+from utils import token_entropy
 
 def average_logprobs(logprobs: list):
     """
@@ -11,3 +12,22 @@ def average_logprobs(logprobs: list):
         return float('-inf')
     return np.mean(logprobs).item()
 
+def average_token_entropy(token_logprobs: list[dict]):
+    """
+    Compute the average token entropy across all tokens.
+
+    Args:
+        token_logprobs (list of dict): A list of token log-probabilities
+    
+    Returns:
+        float: The average token entropy, or -inf if the list is empty
+    """
+    # check if token_logprobs is a list of dicts
+    if not isinstance(token_logprobs, list) or not all(isinstance(item, dict) for item in token_logprobs):
+        raise ValueError("token_logprobs must be a list of dicts")
+    token_entropies = []
+    for logprob in token_logprobs:
+        entropy = token_entropy(logprob)
+        token_entropies.append(entropy)
+    avg_token_entropy = average_logprobs(token_entropies)
+    return avg_token_entropy
